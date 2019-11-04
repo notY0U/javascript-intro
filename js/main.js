fae2f57ec3f51a9f0dc55a135d005ce3361c7608
notY0U <57098864+notY0U@users.noreply.github.com>
2019-11-04 15:33:41 +0200

    js intro

---
+++ js/main.js
@@ -0,0 +1,35 @@
+
+
+// raides 
+console.log('rhs n that');
+
+// skaiciai 
+console.log(3373893);
+
+// boolean - logine reiksme 
+console.log(true);
+console.log(false);
+
+// sarasai - array 
+
+console.log([1,2,3,4]);
+
+// objektai - object
+console.log ({ name: 'me', age:45});
+
+const PI = 3.141592654;
+console.log(PI);
+
+const vardas = 'me';
+console.log(vardas);
+
+let manoAmzius = 34;
+
+manoAmzius = 25;
+console.log(manoAmzius);
+
+var kintamasis = 'x';
+console.log(kintamasis);
+
+
+
